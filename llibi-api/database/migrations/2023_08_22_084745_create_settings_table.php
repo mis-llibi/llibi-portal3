86e45c52e7300c1eaaad114105a9b7778cfad6ce
Glenmore Saga Ilagan <38287498+glenmoreilagan@users.noreply.github.com>
2023-08-22 14:16:41 +0800

    Update 2023_08_22_084745_create_settings_table.php

--- llibi-api/database/migrations/2023_08_22_084745_create_settings_table.php
+++ llibi-api/database/migrations/2023_08_22_084745_create_settings_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
     Schema::create('settings', function (Blueprint $table) {
       $table->id();
       $table->integer('minutes');
-      $table->string('sender');
+      $table->string('receiver');
       $table->timestamps();
     });
   }
