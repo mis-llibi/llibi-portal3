01e6f56d61896cb085e3b7c7ee1531dc8a3417af
Glenmore Saga Ilagan <38287498+glenmoreilagan@users.noreply.github.com>
2023-11-17 21:53:18 +0800

    feedback report add company name and loa type

--- llibi-api/app/Http/Controllers/Feedback/FeedbackController.php
+++ llibi-api/app/Http/Controllers/Feedback/FeedbackController.php
@@ -15,6 +15,7 @@ use App\Services\SendingEmail;
 
 use Illuminate\Http\File;
 use Illuminate\Mail\Attachment;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
@@ -22,10 +23,206 @@ class FeedbackController extends Controller
 {
   public function index()
   {
-    $feedbacks = Feedback::query()->orderBy('id', 'DESC')->paginate(25);
+    $feedbacks = Feedback::query()
+      ->join('app_portal_requests as request', 'request.client_id', '=', 'feedbacks.id')
+      ->leftJoin(env('DB_DATABASE_SYNC') . '.masterlist as mlist', 'mlist.member_id', '=', 'feedbacks.member_id')
+      ->select('feedbacks.*', 'request.loa_type', 'mlist.company_name')
+      ->orderBy('feedbacks.id', 'DESC')->paginate(25);
 
-    return response()->json($feedbacks);
+    $feedback_counts = Feedback::query()->count();
+    $q1 = $this->getStatisticsQuestOne($feedback_counts);
+    $q2 = $this->getStatisticsQuestTwo($feedback_counts);
+    $q3 = $this->getStatisticsQuestThree($feedback_counts);
+    $q4 = $this->getStatisticsQuestFour($feedback_counts);
+
+    return response()->json(['result' => $feedbacks, 'q1' => $q1, 'q2' => $q2, 'q3' => $q3, 'q4' => $q4]);
   }
+
+  private function getStatisticsQuestOne($feedback_counts)
+  {
+    $sql = DB::select("
+      SELECT SUM(very_easy) AS very_easy, SUM(easy) AS easy, SUM(moderate) AS moderate, 
+      SUM(difficult) AS difficult, SUM(very_difficult) AS very_difficult 
+      FROM (
+        SELECT 
+          CASE 
+            WHEN question1 = 5 THEN COUNT(question1)
+            ELSE 0
+            END AS very_easy,
+        CASE 
+            WHEN question1 = 4 THEN COUNT(question1)
+            ELSE 0
+            END AS easy,
+        CASE 
+            WHEN question1 = 3 THEN COUNT(question1)
+            ELSE 0
+            END AS moderate,
+        CASE 
+            WHEN question1 = 2 THEN COUNT(question1)
+            ELSE 0
+            END AS difficult,
+        CASE 
+            WHEN question1 = 1 THEN COUNT(question1)
+            ELSE 0
+            END AS very_difficult
+        FROM feedbacks GROUP BY question1)
+      AS tbl;
+    ");
+
+    $result = [];
+    $hundread_percent = 100;
+    foreach ($sql as $key => $row) {
+      $very_easy = ($row->very_easy / $feedback_counts) * $hundread_percent;
+      $easy = ($row->easy / $feedback_counts) * $hundread_percent;
+      $moderate = ($row->moderate / $feedback_counts) * $hundread_percent;
+      $difficult = ($row->difficult / $feedback_counts) * $hundread_percent;
+      $very_difficult = ($row->very_difficult / $feedback_counts) * $hundread_percent;
+      array_push($result, [
+        'very_easy' => number_format($very_easy, 2),
+        'easy' => number_format($easy, 2),
+        'moderate' => number_format($moderate, 2),
+        'difficult' => number_format($difficult, 2),
+        'very_difficult' => number_format($very_difficult, 2),
+      ]);
+    }
+
+    return $result;
+  }
+
+  private function getStatisticsQuestTwo($feedback_counts)
+  {
+    $sql = DB::select("
+      SELECT SUM(very_easy) AS very_easy, SUM(easy) AS easy, SUM(moderate) AS moderate, 
+      SUM(difficult) AS difficult, SUM(very_difficult) AS very_difficult 
+      FROM (
+        SELECT 
+          CASE 
+            WHEN question2 = 5 THEN COUNT(question2)
+            ELSE 0
+            END AS very_easy,
+        CASE 
+            WHEN question2 = 4 THEN COUNT(question2)
+            ELSE 0
+            END AS easy,
+        CASE 
+            WHEN question2 = 3 THEN COUNT(question2)
+            ELSE 0
+            END AS moderate,
+        CASE 
+            WHEN question2 = 2 THEN COUNT(question2)
+            ELSE 0
+            END AS difficult,
+        CASE 
+            WHEN question2 = 1 THEN COUNT(question2)
+            ELSE 0
+            END AS very_difficult
+        FROM feedbacks GROUP BY question2)
+      AS tbl;
+    ");
+
+    $result = [];
+    $hundread_percent = 100;
+    foreach ($sql as $key => $row) {
+      $very_easy = ($row->very_easy / $feedback_counts) * $hundread_percent;
+      $easy = ($row->easy / $feedback_counts) * $hundread_percent;
+      $moderate = ($row->moderate / $feedback_counts) * $hundread_percent;
+      $difficult = ($row->difficult / $feedback_counts) * $hundread_percent;
+      $very_difficult = ($row->very_difficult / $feedback_counts) * $hundread_percent;
+      array_push($result, [
+        'very_easy' => number_format($very_easy, 2),
+        'easy' => number_format($easy, 2),
+        'moderate' => number_format($moderate, 2),
+        'difficult' => number_format($difficult, 2),
+        'very_difficult' => number_format($very_difficult, 2),
+      ]);
+    }
+
+    return $result;
+  }
+
+  private function getStatisticsQuestThree($feedback_counts)
+  {
+    $sql = DB::select("
+      SELECT SUM(yes) AS yes, SUM(no) AS no
+      FROM (
+        SELECT 
+          CASE 
+            WHEN question3 = 1 THEN COUNT(question3)
+            ELSE 0
+            END AS yes,
+        CASE 
+            WHEN question3 = 0 THEN COUNT(question3)
+            ELSE 0
+            END AS no
+        FROM feedbacks GROUP BY question3)
+      AS tbl;
+    ");
+
+    $result = [];
+    $hundread_percent = 100;
+    foreach ($sql as $key => $row) {
+      $yes = ($row->yes / $feedback_counts) * $hundread_percent;
+      $no = ($row->no / $feedback_counts) * $hundread_percent;
+      array_push($result, [
+        'yes' => number_format($yes, 2),
+        'no' => number_format($no, 2),
+      ]);
+    }
+
+    return $result;
+  }
+
+  private function getStatisticsQuestFour($feedback_counts)
+  {
+    $sql = DB::select("
+      SELECT SUM(very_easy) AS very_easy, SUM(easy) AS easy, SUM(moderate) AS moderate, 
+      SUM(difficult) AS difficult, SUM(very_difficult) AS very_difficult 
+      FROM (
+        SELECT 
+          CASE 
+            WHEN question4 = 5 THEN COUNT(question4)
+            ELSE 0
+            END AS very_easy,
+        CASE 
+            WHEN question4 = 4 THEN COUNT(question4)
+            ELSE 0
+            END AS easy,
+        CASE 
+            WHEN question4 = 3 THEN COUNT(question4)
+            ELSE 0
+            END AS moderate,
+        CASE 
+            WHEN question4 = 2 THEN COUNT(question4)
+            ELSE 0
+            END AS difficult,
+        CASE 
+            WHEN question4 = 1 THEN COUNT(question4)
+            ELSE 0
+            END AS very_difficult
+        FROM feedbacks GROUP BY question4)
+      AS tbl;
+    ");
+
+    $result = [];
+    $hundread_percent = 100;
+    foreach ($sql as $key => $row) {
+      $very_easy = ($row->very_easy / $feedback_counts) * $hundread_percent;
+      $easy = ($row->easy / $feedback_counts) * $hundread_percent;
+      $moderate = ($row->moderate / $feedback_counts) * $hundread_percent;
+      $difficult = ($row->difficult / $feedback_counts) * $hundread_percent;
+      $very_difficult = ($row->very_difficult / $feedback_counts) * $hundread_percent;
+      array_push($result, [
+        'very_easy' => number_format($very_easy, 2),
+        'easy' => number_format($easy, 2),
+        'moderate' => number_format($moderate, 2),
+        'difficult' => number_format($difficult, 2),
+        'very_difficult' => number_format($very_difficult, 2),
+      ]);
+    }
+
+    return $result;
+  }
+
   public function store(FeedbackRequest $request)
   {
     $comment = $request->comment ?? "";
--- llibi-client/src/pages/feedback/client-portal/reports/index.jsx
+++ llibi-client/src/pages/feedback/client-portal/reports/index.jsx
@@ -3,6 +3,128 @@ import React, { useState } from 'react'
 import axios from '@/lib/axios'
 import useSWR from 'swr'
 
+import moment from 'moment'
+
+export const StatsSummaryPanels = ({
+  questionTitle,
+  very_easy,
+  easy,
+  moderate,
+  difficult,
+  very_difficult,
+}) => {
+  return (
+    <div className="bg-white p-3 rounded-md flex-grow basis-[250px]">
+      <div>
+        <small className="font-bold">{questionTitle}</small>
+      </div>
+      <div>
+        <div className="flex justify-between">
+          <div>
+            <small>Very Easy:</small>
+          </div>
+          <div>
+            <small
+              className={`font-bold ${
+                very_easy < 50 ? 'text-red-700' : 'text-green-700'
+              }`}>
+              {very_easy}%
+            </small>
+          </div>
+        </div>
+        <div className="flex justify-between">
+          <div>
+            <small>Easy:</small>
+          </div>
+          <div>
+            <small
+              className={`font-bold ${
+                easy < 50 ? 'text-red-700' : 'text-green-700'
+              }`}>
+              {easy}%
+            </small>
+          </div>
+        </div>
+        <div className="flex justify-between">
+          <div>
+            <small>Moderate:</small>
+          </div>
+          <div>
+            <small
+              className={`font-bold ${
+                moderate < 50 ? 'text-red-700' : 'text-green-700'
+              }`}>
+              {moderate}%
+            </small>
+          </div>
+        </div>
+        <div className="flex justify-between">
+          <div>
+            <small>Difficult:</small>
+          </div>
+          <div>
+            <small
+              className={`font-bold ${
+                difficult < 50 ? 'text-red-700' : 'text-green-700'
+              }`}>
+              {difficult}%
+            </small>
+          </div>
+        </div>
+        <div className="flex justify-between">
+          <div>
+            <small>Very Difficult:</small>
+          </div>
+          <div>
+            <small
+              className={`font-bold ${very_difficult < 50 && 'text-red-700'}`}>
+              {very_difficult}%
+            </small>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export const StatsSummaryPanelsYesNo = ({ questionTitle, yes, no }) => {
+  return (
+    <div className="bg-white p-3 rounded-md flex-grow basis-[250px]">
+      <div>
+        <small className="font-bold">{questionTitle}</small>
+      </div>
+      <div>
+        <div className="flex justify-between">
+          <div>
+            <small>Yes:</small>
+          </div>
+          <div>
+            <small
+              className={`font-bold ${
+                yes < 50 ? 'text-red-700' : 'text-green-700'
+              }`}>
+              {yes}%
+            </small>
+          </div>
+        </div>
+        <div className="flex justify-between">
+          <div>
+            <small>No:</small>
+          </div>
+          <div>
+            <small
+              className={`font-bold ${
+                no < 50 ? 'text-red-700' : 'text-green-700'
+              }`}>
+              {no}%
+            </small>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 export default function ReportHomePage() {
   const [page, setPage] = useState(1)
   const { data: feedbacks, isLoading, isValidating, mutate, error } = useSWR(
@@ -22,74 +144,120 @@ export default function ReportHomePage() {
 
   return (
     <>
-      <nav className="bg-blue-300 h-16">
-        <div className="flex gap-3 px-5">
-          <img src="/logo.png" alt="LLIBI LOGO" width={200} />
-        </div>
-      </nav>
-      <div className="max-w-[65rem] mx-auto text-gray-800">
-        <div className="mb-3 mt-3">
-          <h1 className="uppercase font-bold mb-2">Questions:</h1>
+      <div className="bg-[#f1faee]">
+        <nav className="bg-blue-300 h-16">
+          <div className="flex gap-3 px-5">
+            <img src="/logo.png" alt="LLIBI LOGO" width={200} />
+          </div>
+        </nav>
+        <div className="px-20 text-gray-800">
+          <div className="mb-3 mt-3">
+            <h1 className="uppercase font-bold mb-2">Questions:</h1>
 
-          <p>Q1. How easy was it to use our Client Care Portal?</p>
-          <p>
-            Q2. How easy was it to use our Lacson LOA at the accredited
-            provider?
-          </p>
-          <p>Q3. Did we respond within the turn-around time?</p>
-          <p>Q4. Overall, how satisfied are you with Client Care Portal?</p>
-        </div>
+            <p>Q1. How easy was it to use our Client Care Portal?</p>
+            <p>
+              Q2. How easy was it to use our Lacson LOA at the accredited
+              provider?
+            </p>
+            <p>Q3. Did we respond within the turn-around time?</p>
+            <p>Q4. Overall, how satisfied are you with Client Care Portal?</p>
+          </div>
 
-        <table className="w-full text-sm border">
-          <thead>
-            <tr className="bg-blue-100">
-              <th className="w-48 p-5"></th>
-              <th className="w-72 p-5">Comments</th>
-              <th className="w-24 p-5">Q1</th>
-              <th className="w-24 p-5">Q2</th>
-              <th className="w-24 p-5">Q3</th>
-              <th className="w-24 p-5">Q4</th>
-            </tr>
-          </thead>
-          <tbody>
-            {feedbacks &&
-              feedbacks.data.map(feedback => {
-                return (
-                  <tr key={feedback.id} className="border even:bg-gray-50">
-                    <td className="p-5 font-bold">
-                      <small>{feedback.company_code}</small>
-                      <br />
-                      {feedback.member_id}
-                    </td>
-                    <td className="p-5">{feedback.comments}</td>
-                    <td className="p-5 text-center">{feedback.question1}</td>
-                    <td className="p-5 text-center">{feedback.question2}</td>
-                    <td className="p-5 text-center">{feedback.question3}</td>
-                    <td className="p-5 text-center">{feedback.question4}</td>
-                  </tr>
-                )
-              })}
-          </tbody>
-        </table>
-        <div className="flex justify-end">
-          <div>
-            <button
-              className={`w-28 border bg-blue-900 p-2 uppercase text-sm rounded-md text-white mb-3 ${
-                feedbacks?.current_page === 1 && 'bg-gray-600'
-              }`}
-              onClick={() => setPage(page - 1)}
-              disabled={feedbacks?.current_page === 1}>
-              Previous
-            </button>
-            <button
-              className={`w-28 border bg-blue-900 p-2 uppercase text-sm rounded-md text-white mb-3 ${
-                feedbacks?.current_page === feedbacks?.last_page &&
-                'bg-gray-600'
-              }`}
-              onClick={() => setPage(page + 1)}
-              disabled={feedbacks?.current_page === feedbacks?.last_page}>
-              Next
-            </button>
+          <div className="mb-3">
+            <h1 className="uppercase font-bold mb-2">Statistics:</h1>
+            <div className="flex gap-3 flex-wrap">
+              <StatsSummaryPanels
+                questionTitle={'Question 1.'}
+                very_easy={feedbacks?.q1[0]?.very_easy}
+                easy={feedbacks?.q1[0]?.easy}
+                moderate={feedbacks?.q1[0]?.moderate}
+                difficult={feedbacks?.q1[0]?.difficult}
+                very_difficult={feedbacks?.q1[0]?.very_difficult}
+              />
+              <StatsSummaryPanels
+                questionTitle={'Question 2.'}
+                very_easy={feedbacks?.q2[0]?.very_easy}
+                easy={feedbacks?.q2[0]?.easy}
+                moderate={feedbacks?.q2[0]?.moderate}
+                difficult={feedbacks?.q2[0]?.difficult}
+                very_difficult={feedbacks?.q2[0]?.very_difficult}
+              />
+              <StatsSummaryPanelsYesNo
+                questionTitle={'Question 3.'}
+                yes={feedbacks?.q3[0]?.yes}
+                no={feedbacks?.q3[0]?.no}
+              />
+              <StatsSummaryPanels
+                questionTitle={'Question 4.'}
+                very_easy={feedbacks?.q4[0]?.very_easy}
+                easy={feedbacks?.q4[0]?.easy}
+                moderate={feedbacks?.q4[0]?.moderate}
+                difficult={feedbacks?.q4[0]?.difficult}
+                very_difficult={feedbacks?.q4[0]?.very_difficult}
+              />
+            </div>
+          </div>
+
+          <table className="w-full text-sm border bg-white mb-3">
+            <thead>
+              <tr className="bg-blue-100">
+                <th className="w-48 p-5"></th>
+                <th className="w-24 p-5">Loa Type</th>
+                <th className="w-72 p-5">Comments</th>
+                <th className="w-24 p-5">Q1</th>
+                <th className="w-24 p-5">Q2</th>
+                <th className="w-24 p-5">Q3</th>
+                <th className="w-24 p-5">Q4</th>
+              </tr>
+            </thead>
+            <tbody>
+              {feedbacks &&
+                feedbacks?.result?.data.map(feedback => {
+                  return (
+                    <tr key={feedback.id} className="border even:bg-gray-100">
+                      <td className="p-5 font-bold">
+                        <small className="text-gray-500">
+                          {moment(feedback.created_at).format('MMM D, Y')}
+                        </small>
+                        <br />
+                        <span>{feedback.company_name}</span>
+                      </td>
+                      <td className="p-5 text-center">
+                        {feedback.loa_type.toUpperCase()}
+                      </td>
+                      <td className="p-5">{feedback.comments}</td>
+                      <td className="p-5 text-center">{feedback.question1}</td>
+                      <td className="p-5 text-center">{feedback.question2}</td>
+                      <td className="p-5 text-center">{feedback.question3}</td>
+                      <td className="p-5 text-center">{feedback.question4}</td>
+                    </tr>
+                  )
+                })}
+            </tbody>
+          </table>
+          <div className="flex justify-end">
+            <div>
+              <button
+                className={`w-28 border bg-blue-900 p-2 uppercase text-sm rounded-md text-white mb-3 ${
+                  feedbacks?.result?.current_page === 1 && 'bg-gray-600'
+                }`}
+                onClick={() => setPage(page - 1)}
+                disabled={feedbacks?.result?.current_page === 1}>
+                Previous
+              </button>
+              <button
+                className={`w-28 border bg-blue-900 p-2 uppercase text-sm rounded-md text-white mb-3 ${
+                  feedbacks?.result?.current_page ===
+                    feedbacks?.result?.last_page && 'bg-gray-600'
+                }`}
+                onClick={() => setPage(page + 1)}
+                disabled={
+                  feedbacks?.result?.current_page ===
+                  feedbacks?.result?.last_page
+                }>
+                Next
+              </button>
+            </div>
           </div>
         </div>
       </div>
