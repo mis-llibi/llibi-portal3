907f26f6b9ea40e2c18129b53c27fe172d5d3f4e
ShiinaNeet <72781583+ShiinaNeet@users.noreply.github.com>
2025-04-21 15:29:14 +0800

    Function Created Search Birthdate By Name

--- llibi-api/app/Http/Controllers/SearchMasterlist/MasterlistController.php
+++ llibi-api/app/Http/Controllers/SearchMasterlist/MasterlistController.php
@@ -26,4 +26,37 @@ class MasterlistController extends Controller
 
     return $clients;
   }
+  public function SearchBirthdateByName(Request $request){
+
+    $lastName = $request->input('last_name');
+    $firstName = $request->input('first_name');
+    $offset = $request->input('offset', 0);
+    $limit = $request->input('limit', 50);
+    $clients = Sync::query()
+      // ->whereNotNull('last_name')
+      ->where(function ($q) use ($firstName, $lastName) {
+          // If both names are provided, match in both orders
+          if ($firstName && $lastName) {
+              $q->where(function ($subQ) use ($firstName, $lastName) {
+                  $subQ->whereRaw('UPPER(first_name) LIKE ?', ["%$firstName%"])
+                      ->whereRaw('UPPER(last_name) LIKE ?', ["%$lastName%"]);
+              })->orWhere(function ($subQ) use ($firstName, $lastName) {
+                  $subQ->whereRaw('UPPER(first_name) LIKE ?', ["%$lastName%"])
+                      ->whereRaw('UPPER(last_name) LIKE ?', ["%$firstName%"]);
+              });
+          }
+          // If only one is provided, search either field
+          elseif ($firstName || $lastName) {
+              $term = strtoupper($firstName ?: $lastName);
+              $q->whereRaw('UPPER(first_name) LIKE ?', ["%$term%"])
+                ->orWhereRaw('UPPER(last_name) LIKE ?', ["%$term%"]);
+          }
+      })
+      ->offset($offset)
+      ->limit($limit)
+      ->get();
+
+
+    return response()->json($clients);
+  }
 }
