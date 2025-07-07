f137502d6e3b4593df4d59f130646d6539ea0e4d
0ojireho0 <jeremiahquintano17@gmail.com>
2025-07-07 10:04:13 +0800

    terms of use word changing

--- llibi-client/src/pages/self-service/client/index.js
+++ llibi-client/src/pages/self-service/client/index.js
@@ -173,56 +173,64 @@ const Client = () => {
             <p className="text-justify">
               Please read these Terms of Use before using the <b>LLIBI Client Care Portal ("Portal").</b><br /><br />
 
-              By continuing to use this Portal, you hereby certify that you have read, reviewed, and agreed to comply with the terms indicated below. <br /><br />
+              By using this Portal, you explicitly consent and agree to comply with the terms set forth below. If you do not agree with these terms, please discontinue your use of the Portal. <br /><br />
 
-              Use of this <i>Portal</i> is optional. If you do not agree with the terms, you may discontinue use. <br /><br />
+              <b>Terms of Use</b><br />
 
-              This <i>Portal</i>, created, owned, and managed by Lacson and Lacson Insurance Brokers, Inc. (“LLIBI”/us/our), aims to provide online-based services to facilitate requests and transactions of our MEMBERS. <br /><br />
+              This <i>Portal</i> is created, owned, and managed by Lacson and Lacson Insurance Brokers, Inc. ("LLIBI"/us/our), to facilitiate online-based services and transactions for our MEMBERS.<br /><br />
 
-              This <i>Portal</i> allows you, the User, to do the following:
+              Through this Portal, you may: <br />
             </p>
             <ul>
               <li>
-                • Submit an online request for a letter of authorization,
-                callback;
+                • Submit online requests for letters of authorization or callbacks;
               </li>
               <li>
-                • Upload copies of your doctor’s request, medical certificate, and other documents to facilitate your request;
+                • Upload necessary documentation such as doctor's request and medical certificates;
               </li>
               <li>
-                • Confirm your active coverage by providing your membership
-                data; and
+                • Confirm active coverage by providing membership data; and
               </li>
               <li>
-                • Provide relevant data in connection to your request.
+                • Provide relevant data required to process your requests.
               </li>
             </ul>
             <br />
             <p className="text-justify">
               No enrolment is necessary for use of this <i>Portal</i>.<br /><br />
 
-              While LLIBI implements appropriate security measures, you are expected to use this <i>Portal</i> only for the above purposes. At the same time, you are responsible for protecting the confidentiality of the information you provide including your date of birth, emergency card number, and other information required for the use of this <i>Portal</i>. <br /><br />
+              While LLIBI implements appropriate security measures, you are expected to use this Portal only for the above purposes. At the same time, you are responsible for safeguarding the confidentiality of the information you provide including your date of birth, emergency card number, and other information required for the use of this <i>Portal</i>.<br /><br />
 
-              We will never ask for payment information such as bank account numbers, credit card numbers, and the likes via this <i>Portal</i>.<br /><br />
+              LLIBI will never ask for payment information (such as bank or credit card details) via this <i>Portal</i>. Please report immediately if you encounter such requests.<br /><br />
 
-              <span>By using this <i>Portal</i>, you also agree that any information provided will be shared with necessary LLIBI systems for processing your request.</span><br /><br />
+              By using this <i>Portal</i>, you also agree that any information provided will be shared with necessary LLIBI systems for processing your request.<br /><br />
+
+              LLIBI reserves the right to deny access or services through the Portal if fraud is suspected, or if necessary, to conduct investigation.<br /><br />
 
               <b>Privacy Policy</b><br />
 
-              The use of this <i>Portal</i> and any data collected herein are in accordance with Philippine laws, including the Rules and Regulations of the Republic Act No. 10173 or the Data Privacy Act of 2012.<br /><br />
+              Your use of the <i>Portal</i> and the personal data provided herein shall be processed strictly in compliance with Republic Act No. 10173, known as the Data Privacy Act of 2012, and its implementing rules and regulations.<br /><br />
 
-              No personal data will be stored in this <i>Portal</i>. Further, to maintain the confidentiality of members’ data, and to provide you with continuous services through this <i>Portal</i>, you are expected to provide accurate information. Users are prohibited from using data mining tools.<br /><br />
+              No personal data will be stored within this <i>Portal</i>. Further, to maintain the confidentiality of members’ data, and to provide you with continuous services through this <i>Portal</i>, you are expected to provide accurate information. Users are prohibited from using data mining tools.<br /><br />
 
               LLIBI reserves the right to decline access or services to this <i>Portal</i> in case of suspected fraud or to facilitate investigation of similar cases.<br /><br />
 
-              Your personal data will be shared only to those with a business need to know. LLIBI will retain your data and transaction records for a period of five (5) years from submission, or in accordance to your consent, whichever is appropriate or in compliance with applicable regulations.<br /><br />
+              Your personal data will only be shared internally within LLIBI’s systems on a strict “need-to-know” basis.  LLIBI will retain your data and transaction records for a period of five (5) years from submission, or in accordance to your consent, whichever is appropriate or in compliance with applicable regulations.<br /><br />
 
-              For more information on how LLIBI handles your personal data, please visit our Privacy Notice at <a href='https://llibi.com/data-privacy/' target='_blank' className='text-blue-700'>https://llibi.com/data-privacy/</a>.<br /><br />
+              You are guaranteed the rights provided under the Data Privacy Act, including the right to access, correction, objection, data portability, and the right to lodge a complaint with the National Privacy Commission (NPC).<br /><br />
 
-              If you have questions, or would like to report a personal data breach, or if you believe your personal data have been compromised, please email <a className='text-blue-700' href='emailto:privacy@llibi.com'>privacy@llibi.com.</a><br /><br />
+              For more information on our data handling practices, or if you need assistance regarding your privacy rights, please visit our Privacy Notice at <a href='https://llibi.com/data-privacy/' target='_blank' className='text-blue-700'>https://llibi.com/data-privacy/</a>.<br /><br />
 
               <b>Liability</b><br />
-              <span>LLIBI is not liable for damages that may happen as a result of your misuse of this Portal. </span>
+              LLIBI shall not be liable for any damages or losses arising from your misuse of this <i>Portal</i>, or from circumstances beyond our control, such as unauthorized access resulting from your failure to adequately protect your personal information.<br /><br />
+
+              <b>Changes to these Terms</b><br />
+              We may modify these Terms of Use from time to time. Any updates will be clearly posted on this <i>Portal</i>, and your continued use after any changes signifies your acceptance of the updated terms.<br /><br />
+
+              <b>Explicit Consent</b><br />
+              By actively submitting information or proceeding with requests via this <i>Portal</i>, you explicitly consent to the collection, processing, and sharing of your personal as described in these Terms and our Privacy Policy.<br /><br />
+
+              If you have any questions regarding these Terms, would like to report a personal data breach, if you believe your personal data have been compromised , or wish to withdraw your consent at any time, please contact us at <a className='text-blue-700' href='mailto:privacy@llibi.com'>privacy@llibi.com.</a>.
 
             </p>
             <div className="text-center mt-4 gap-3 flex justify-center items-center">
